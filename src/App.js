c576a461082183055684ab55980d6c48885aea1c
irshad <irshadapcvl@gmail.com>
2022-08-20 14:07:27 +0530

    netflix

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import requests from './request'
+import Row from './Row';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Row
+        title="NETFLIX ORGINALS"
+        fetchUrl={requests.fetchNetflixOriginals}/>
+      <Row title="Trending Now" frtchUrl={requests.fetchTrending}/>
+     
     </div>
   );
 }
